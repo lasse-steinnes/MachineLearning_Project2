6b4715b69410123f70a70d96284300991e1ed698
Lukas Materne <lukas_materne@yahoo.de>
2019-10-14 12:51:01 +0200

    added test cases for helper functions

---
+++ test.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import numpy as np 
+
+from helper_functions import one_hot, model
+
+filename = "default of credit card clients.xls"
+df = pd.read_excel(filename, header=1)
+
+def test_one_hot():
+    y = df["PAY_0"].to_numpy()
+
+    l_uni = len(np.unique(y))
+    l_y = len(y)
+
+    y_hot, y_key = one_hot(y)
+
+    y_decode = np.zeros(l_y)
+    for i in range(l_y):
+        y_decode[i] = y_key[np.argmax(y_hot[i])]
+
+    assert np.all(y_decode==y)
+    assert np.all(y_hot.shape == (l_y,l_uni))
+
+def test_model():
+    x = df.drop(columns =["PAY_0"]).to_numpy()
+    X = model(x,1)
+    assert np.all( X.shape == df.shape)
+    assert np.all( X[:,0] - 1 == 0 )
