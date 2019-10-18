47007de5d6bc1a8cb65e67de87a018bf558c3d23
Lukas Materne <lukas_materne@yahoo.de>
2019-10-18 16:20:21 +0200

    seperated one hot encoding, update of confusion matrix

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,8 +1,10 @@
 import autograd.numpy as np
+from sklearn.model_selection import train_test_split
 import pandas as pd 
 from SGD import SGD
+from helper_functions import OneHot
 
-class LogisticRegression (SGD):
+class LogisticRegression (SGD, OneHot):
     """
     class that performs logistic regression with n classes to classify
     Methods:
@@ -32,13 +34,7 @@ class LogisticRegression (SGD):
         logging: if True keep log of all updates
         """
         self.classes = classes
-        if class_dict != None:
-            self.one_hot_encoding_key = class_dict
-            self.one_hot_decoding_key = {index: value for index, value in class_dict.items()}
-            self.__provided_dict =True
-        else:
-            self.__provided_dict =False
-      
+        OneHot.__init__(self, dictonary=class_dict)      
         SGD.__init__(self, LogisticRegression.__cross_entropy, epochs =epochs, mini_batch_size = mini_batch_size,
                      learning_rate = learning_rate, adaptive_learning_rate = adaptive_learning_rate, tolerance = tol, max_iter = max_iter)
         self.__fit_count = 0
@@ -49,23 +45,38 @@ class LogisticRegression (SGD):
             self.logs = pd.DataFrame(columns=["Fit nr","epoch", "data set", "mse", "r2", "accuracy"])
             self.__log_calls = 0
 
-    def fit(self, X, y):
+    def fit(self, X, y, split = False , fraction = 0.2):
         self.__fit_count += 1
+        if split:
+            X, X_test, y, y_test = train_test_split(X, y, test_size = fraction)
+        else:
+            X_test, y_test = X, y
         #convert to one hot encoding 
-        y_one_hot = LogisticRegression.__one_hot_encoding(self, y)
-        self.weights = np.random.rand(X.shape[1]* self.classes).reshape((X.shape[1], self.classes))  # initialization
-        old = self.weights
+        y_one_hot = OneHot.encoding(self, y)
+        self.weights = 10**(-3)*np.random.rand(X.shape[1]* self.classes).reshape((X.shape[1], self.classes))  # initialization
+        best_weights = np.copy(self.weights)
+        best_acc = 0
         #sgd
         if self.log:
             for self.current_epoch in range(0, self.log_epochs):
                 SGD.run_SGD(self, X, y_one_hot)
                 score = LogisticRegression.evaluate(self, X, y, data_set="train")
-                print(np.linalg.norm(self.weights))
-                print("Epoch %i " %self.current_epoch, [(met , " %.2f" %sc)  for met,sc in score.items()])
+                if split:
+                    score = LogisticRegression.evaluate(self, X_test, y_test, data_set="test")
+                sc =score["accuracy"]
+                if sc > best_acc:
+                    best_weights = np.copy(self.weights)
+                print("Epoch %i " %self.current_epoch, "accuracy: %.2f" %  sc)
         else:
             SGD.run_SGD(self, X, y_one_hot)
+
         #evaluate
-        score = LogisticRegression.evaluate(self, X, y, data_set="train")
+        if split:
+            final_eval = "test"
+            self.weights = best_weights
+        else:
+            final_eval ="train"
+        score = LogisticRegression.evaluate(self, X_test, y_test, data_set=final_eval)
         return score
 
     def predict(self, X, decoded = False):
@@ -74,13 +85,13 @@ class LogisticRegression (SGD):
         nom = np.sum( np.exp(z))
         p = np.exp(z) / nom
         if decoded:
-            return LogisticRegression.__one_hot_decoding(self, p)
+            return OneHot.decoding(self, p)
         return p
 
     def evaluate(self, X, y, data_set= "test"):
         prediction = LogisticRegression.predict(self, X)
         
-        pred_class = LogisticRegression.__one_hot_decoding(self, prediction)
+        pred_class = OneHot.decoding(self, prediction)
 
         scores = {'mse' : LogisticRegression.__MSE(self,pred_class, y),
                   'r2': LogisticRegression.__R2(self,pred_class, y),
@@ -99,7 +110,7 @@ class LogisticRegression (SGD):
         returns the confusion matrix, i.e. number of true positives and flase negatives for all classes
         """
         prediction = LogisticRegression.predict(self, X)
-        prediction = LogisticRegression.__one_hot_decoding(self, prediction)
+        prediction = OneHot.decoding(self, prediction)
         
         list_of_classes = np.array(list(self.one_hot_encoding_key))
         list_of_classes = list_of_classes[:,np.newaxis]
@@ -121,14 +132,17 @@ class LogisticRegression (SGD):
         
         metrics = [P,R,S,A]
 
-        input_df = np.zeros( (self.classes, self.classes +4))
-        input_df[:,:self.classes] = matrix
+        norm = np.sum(matrix)
+        input_df = np.zeros( (self.classes +1, self.classes +4))
+        input_df[:-1,:self.classes] = matrix
+        input_df[-1, :self.classes] = np.sum(matrix,axis = 1)/norm
+
         for i, val in enumerate(metrics):
-            input_df[:,-4 + i] = val
+            input_df[:-1,-4 + i] = val
 
         confusion = pd.DataFrame(input_df,
                                  columns = np.append([ str(self.one_hot_decoding_key[i]) for i in range(self.classes)], ["precision", "recall", "specificity", "accuracy"]),
-                                 index = [self.one_hot_decoding_key[i] for i in range(self.classes)])
+                                 index = np.append([self.one_hot_decoding_key[i] for i in range(self.classes)],'occurence'))
         confusion.index.name = 'predicted class'
         confusion.columns.name = 'actual class'
         return confusion
@@ -141,40 +155,6 @@ class LogisticRegression (SGD):
         nom = np.sum( np.exp(z))
         prediction = np.exp(z) / nom
         return - np.sum(np.where(y ==1,np.log(prediction), 0))/len(y)
-
-    def __one_hot_encoding(self, y):
-        """
-        computes the one hot encding for the vector y
-        returns y in shape (samples, #unique instances)
-        """
-        uni = np.unique(y)
-        l_uni = len(uni)
-        if l_uni != self.classes:
-            print("Not all classes in training data!")
-        
-        l_y = len(y)
-        hot = np.zeros((l_y, l_uni))
-        #inferr dict only at first call otherwise it is provided from class
-
-        if (self.__fit_count == 1) and not self.__provided_dict:
-            self.one_hot_encoding_key = {uni[i]: i for i in range(l_uni)}
-            self.one_hot_decoding_key = {i:uni[i] for i in range(l_uni)}
-            
-        for i in range(l_y):
-            index  = self.one_hot_encoding_key[y[i]]
-            hot[i, index] = 1
-        return hot 
-
-    def __one_hot_decoding(self, y):
-        """
-        decode one hot encoding of prediction
-        """
-        l_y = len(y)
-        pred_class = np.zeros(l_y)
-       
-        for i in range(l_y):
-            pred_class[i] = self.one_hot_decoding_key[np.argmax(y[i])] 
-        return pred_class
 
     #MSE; R2; accuracy
     def __MSE(self, prediction, y):
