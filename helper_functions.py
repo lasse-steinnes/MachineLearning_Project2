40c3046b44e969b12a10c1c9920c63289ba49973
Lukas Materne <lukas_materne@yahoo.de>
2019-10-18 16:19:17 +0200

    OneHot encoding as seperate class"

--- helper_functions.py
+++ helper_functions.py
@@ -14,25 +14,6 @@ def model(data, power):
     ret [1:] = data.T
     return ret.T
 
-def one_hot(y):
-    """
-    computes the one hot encding for the vector y
-    returns y in shape (samples, #unique instances)
-    """
-    uni = np.unique(y)
-    l_uni = len(uni)
-    l_y = len(y)
-    hot = np.zeros((l_y, l_uni))
-
-    encoding = {uni[i]: i for i in range(l_uni)}
-    decoding = {i:uni[i] for i in range(l_uni)}
-    
-    for i in range(l_y):
-        index  = encoding[y[i]]
-        hot[i, index] = 1
-    return hot, decoding
-
-
 def parse_data(df, target, power = 1, unbalanced = True):
     """
     df: pandas.dataframe
@@ -44,11 +25,14 @@ def parse_data(df, target, power = 1, unbalanced = True):
     """
     if unbalanced:
         y = df[target].to_numpy()
-        y,c = np.unique(y,return_counts=True)
-        drop = int(c[y[0]] - c[y[1]])
+        _,c = np.unique(y,return_counts=True)
+        drop = int(c[0]- c[1])
+        print(drop)
         df = df.sort_values(by = [target])
-        df = df.drop(index = np.arange(0,drop))
-        df = df.sample(frac=1, replace=True)
+        df.index = np.arange(0,len(y))
+        df = df.drop(labels = np.arange(0,drop), axis = 0)
+        y = df[target].to_numpy()
+        df = df.sample(frac=1, replace=False)
 
     y = df[target].to_numpy()
     df  = df.drop(columns=[target])
@@ -60,6 +44,45 @@ def parse_data(df, target, power = 1, unbalanced = True):
 
     return X, y
     
+class OneHot:
+    """
+    class that provides one hot encoding for classification problems
+    """
+    def __init__(self, dictonary = None):
+        if dictonary != None:
+            self.one_hot_encoding_key = dictonary
+            self.one_hot_decoding_key = {index: value for index, value in dictonary.items()}
+            self.provided_dict =True
+        else:
+            self.provided_dict =False
     
+    def encoding(self, y):
+        """
+        computes the one hot encding for the vector y
+        returns y in shape (samples, #unique instances)
+        """
+        uni = np.unique(y)
+        l_uni = len(uni)       
+        l_y = len(y)
+        hot = np.zeros((l_y, l_uni))
+        #inferr dict only at first call otherwise it is provided from class
+        if not self.provided_dict:
+            self.one_hot_encoding_key = {uni[i]: i for i in range(l_uni)}
+            self.one_hot_decoding_key = {i:uni[i] for i in range(l_uni)}
+            self.provided_dict = True
+        #actuall encoding
+        for i in range(l_y):
+            index  = self.one_hot_encoding_key[y[i]]
+            hot[i, index] = 1
+        return hot 
 
+    def decoding(self, y):
+        """
+        decode one hot encoding of prediction
+        """
+        l_y = len(y)
+        pred_class = np.zeros(l_y)
+        for i in range(l_y):
+            pred_class[i] = self.one_hot_decoding_key[np.argmax(y[i])] 
+        return pred_class
 
