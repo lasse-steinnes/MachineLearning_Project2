bug fixes in normalization

LogisticRegression.py
@@ -39,7 +39,7 @@ class LogisticRegression (SGD, OneHot):
 
         OneHot.__init__(self, dictonary=class_dict)      
         SGD.__init__(self, LogisticRegression.__cross_entropy, epochs =epochs, mini_batch_size = mini_batch_size,
-                     learning_rate = learning_rate, adaptive_learning_rate = adaptive_learning_rate, tolerance = tol, max_iter = max_iter)
+                     learning_rate = learning_rate, adaptive_learning_rate = adaptive_learning_rate, tolerance = tol)
         
         self.__fit_count = 0
         self.log = logging
@@ -91,8 +91,6 @@ class LogisticRegression (SGD, OneHot):
             #training without logging
             SGD.run_SGD(self, X, y_one_hot)
 
-        #reset for netx fit
-        self.iterations = 0
         #use best par
         if split:
             self.weights = best_weights
@@ -101,7 +99,7 @@ class LogisticRegression (SGD, OneHot):
     def predict(self, X, decoded = False):
         z = X@self.weights
         #softmax function
-        nom = np.sum( np.exp(z))
+        nom = np.vstack((np.sum( np.exp(z), axis= 1)) * self.classes)
         p = np.exp(z) / nom
         if decoded:
             return OneHot.decoding(self, p)
@@ -130,7 +128,7 @@ class LogisticRegression (SGD, OneHot):
     def __cross_entropy(self,W, X, y):
         z = X@W
         #softmax function
-        nom = np.sum( np.exp(z))
+        nom = np.vstack((np.sum( np.exp(z), axis= 1)) * self.classes)
         prediction = np.exp(z) / nom
         ret = - np.sum(np.where(y ==1, np.log(prediction), 0))/len(y)
         if self.reg[0] == 'l1':
@@ -149,7 +147,7 @@ class LogisticRegression (SGD, OneHot):
         W = np.zeros(self.weights.shape)
         W[0] = self.weights[0]
         z = X@W
-        nom = np.sum(np.exp(z))
+        nom = np.vstack((np.sum( np.exp(z), axis= 1)) * self.classes)
         pred = np.exp(z)/nom
         L0 = np.sum(np.log(pred))
         LB = np.sum(np.log(LogisticRegression.predict(self, X)))

SGD.py
@@ -10,7 +10,8 @@ class SGD:
     '''
     
     def __init__(self, cost_function, epochs =10, mini_batch_size = 10, 
-                learning_rate = 0.5, adaptive_learning_rate = 'const', tolerance = 1, max_iter = 1000):
+                learning_rate = 0.5, adaptive_learning_rate = 'const', momentum = True,
+                tolerance = 1):
         """
         Stocastic gradient descent (SGD) should be run
         in each batch. It should be used by picking a few points
@@ -34,11 +35,11 @@ class SGD:
         self.learning_rate = learning_rate
 
         self.tolerance = tolerance
-        self.max_iter = max_iter
-        #tracks number of weight updates
-        self.iteration =0
         #tracks number of epochs
         self. time = 0
+        self.momentum = momentum
+        if momentum:
+            self.v = 0
 
         self.weights = None
         
@@ -52,7 +53,7 @@ class SGD:
         num_mini_batches = samples // self.mini_batch_size
         self.__old_weights = 100
        
-        while SGD.__check(self, self.iteration, self.__old_weights):
+        while SGD.__check(self, self.__old_weights):
             SGD.run_epoch(self,X, t,num_mini_batches)
             self.time += 1
         return self.weights
@@ -68,6 +69,8 @@ class SGD:
     
         self.delta = self.deriv_cost_function(self, self.weights, minibatch[0], minibatch[1])
         self.__old_weights = self.weights
+        if self.momentum:
+            pass
         if update_weight:
             self.weights = self.__old_weights - self.gamma * self.delta
 
@@ -80,7 +83,6 @@ class SGD:
         """
         for mini_batch in SGD.creat_mini_batch(self, X, t, num_mini_batches):
             SGD.run_minibatch(self, mini_batch)
-            self.iteration += 1
         self.time += 1
         
 
@@ -88,7 +90,7 @@ class SGD:
         """
         returns array with [... (X_n, t_n) ...] for 0 < n < num_mini_batches
         """
-        cur_seed = np.random.randint(self.epochs*self.max_iter + self.iteration)
+        cur_seed = np.random.randint(self.epochs*10**4)
         np.random.seed(cur_seed)
         np.random.shuffle(X)
         np.random.seed(cur_seed)
@@ -101,9 +103,6 @@ class SGD:
     
     #halting condition
     def __check(self, iteration, old_weights):
-        if iteration >= self.max_iter: 
-            print("Max. iter. reached")
-            return False
         if np.linalg.norm(old_weights - self.weights) < self.tolerance:
             print("tolerance reached")
             return False
@@ -115,4 +114,5 @@ class SGD:
         return gamma0 / ( gamma0*t +1)
         
     def __momentum(self, m0, t):
-        pass
+        self.v = self.v * m0 + (1-m0)*self.delta
+